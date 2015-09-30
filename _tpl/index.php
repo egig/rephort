85ae6ab06fb05ab1c6f4591acddc3c281b9f3b2c
egig <egigundari@gmail.com>
2015-09-30 12:04:50 +0700

    add bootstrap

--- _tpl/index.php
+++ _tpl/index.php
@@ -1,87 +1,80 @@
-<h1>Tasks Assignment Diagram</h1>
 
-<ul>
-	<li>Height of row indicates number of tasks.</li>
-	<li> Different color of cells indicates different projects.</li>
-	<li> Click cells to go to the task.</li>
-</ul>
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<table border="1" style="border-collapse: collapse;">
-	<thead>
-		<tr>
-			<th rowspan="2">User</th>
-			<th colspan="<?= ($days+1) ?>"> <?php echo date('F Y') ?></th>
-		</tr>
-		<tr>
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+		<link rel="stylesheet" type="text/css" href="<?= $base_path ?>/assets/plugins/bootstrap/dist/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="<?= $base_path ?>/assets/css/style.css">
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <div class="container">
+	        <div class="row">
+		        <div class="col-md-12">
+					<h1>Tasks Assignment Diagram</h1>
+
+					<ul>
+						<li>Height of row indicates number of tasks.</li>
+						<li> Different color of cells indicates different projects.</li>
+						<li> Click cells to go to the task.</li>
+					</ul>
+		        </div>
+	        </div>
+	        <div class="row">
+		        <div class="col-md-12">
+		        	<table class="table table-bordered">
+						<thead>
+							<tr>
+								<th rowspan="2">User</th>
+								<th colspan="<?= ($days+1) ?>"> <?php echo date('F Y') ?></th>
+							</tr>
+							<tr>
+								<?php foreach (range(1, $days) as $day): ?>
+									<th style="width:30px;"><?= $day ?></th>
+								<?php endforeach ?>
+							</tr>
+						</thead>
+						<tbody>
+							<?php foreach ($users as $user): ?>
+								<tr id="row-<?= $user['phid'] ?>">
+									<td valign="top" style="padding:5px 10px">
+										<img style="height:20px;" src="<?php echo $user['image'] ?>">
+										<?= $user['realName'] ?>
+									</td>
+									<?php foreach (range(1, $days) as $day): ?>
+										<td <?php if($day == date('j') ): ?> style="background:#e1e1e1" <?php endif ?>  class="day day-<?php echo mktime(0,0,0, date('n'), $day, date('Y')) ?>"></td>
+									<?php endforeach ?>
+								</tr>
+							<?php endforeach ?>
+						</tbody>
+					</table>
+		        </div>
+	        </div>
+        </div>
+			
+		<script type="text/javascript" src="<?= $base_path ?>/assets/plugins/jquery/dist/jquery.js"></script>
+		<script type="text/javascript" src="<?= $base_path ?>/assets/plugins/bootstrap/dist/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="<?= $base_path ?>/assets/js/app.js"></script>
+		<script type="text/javascript">
+
+			var baseUrl = "<?= $base_path ?>";
+
+			var days = [];
 			<?php foreach (range(1, $days) as $day): ?>
-				<th style="width:30px;"><?= $day ?></th>
+				days.push(<?= mktime(0,0,0, date('n'), $day, date('Y')) ?>);
 			<?php endforeach ?>
-		</tr>
-	</thead>
-	<tbody>
-		<?php foreach ($users as $user): ?>
-			<tr id="row-<?= $user['phid'] ?>">
-				<td valign="top" style="padding:5px 10px">
-					<img style="height:20px;" src="<?php echo $user['image'] ?>">
-					<?= $user['realName'] ?>
-				</td>
-				<?php foreach (range(1, $days) as $day): ?>
-					<td <?php if($day == date('j') ): ?> style="background:gray" <?php endif ?>  class="day-<?php echo mktime(0,0,0, date('n'), $day, date('Y')) ?>"></td>
-				<?php endforeach ?>
-			</tr>
-		<?php endforeach ?>
-	</tbody>
-</table>
-	
-<script type="text/javascript" src="<?= $base_path ?>/assets/plugins/jquery/dist/jquery.js"></script>
-<script type="text/javascript">
 
-	var days = [];
-	<?php foreach (range(1, $days) as $day): ?>
-		days.push(<?= mktime(0,0,0, date('n'), $day, date('Y')) ?>);
-	<?php endforeach ?>
-
-	$(function(){
-		$.ajax({
-			url: "<?= $base_path ?>/api",
-			type: "GET",
-			success: function(response) {
-
-				var users = response.users;
-				for(var i in users) {
-
-					for( projectPHID in users[i].targeted_tasks) {
-
-						var tasks =  users[i].targeted_tasks[projectPHID];
-
-						for(var x in tasks) {
-
-							for(var n in days) {
-
-								// remove hour/minuts/secends
-								var xd = new Date(tasks[x].dateCreated*1000);
-								var dd = new Date(xd.getFullYear(), xd.getMonth(), xd.getDate())
-
-								var xtd = new Date(tasks[x]['auxiliary']['std:maniphest:creasindo:target']*1000);
-								var dtd = new Date(xtd.getFullYear(), xtd.getMonth(), xtd.getDate())
-
-								var url = 'javascript:;';
-								var title = '';
-								var bg = 'transparent';
-
-								if(days[n] >= dd.getTime()/1000 && days[n] <= dtd.getTime()/1000 ) {
-									title = tasks[x].objectName;
-									url = tasks[x].uri;
-									bg = response.projectColors[projectPHID];
-								}
-
-								$('#row-'+users[i].phid).find('.day-'+days[n])
-									.append('<a href="'+url+'"  target="_blank" ><div title="'+title+'" style="font-size:0.7em;width:30px;height:10px;background-color:'+bg+'"></div></a>');
-							}
-						}
-					}
-				}
-			}
-		});
-	});
-</script>
+			App.init(baseUrl, days);
+		</script>
+    </body>
+</html>
